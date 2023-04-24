afc1e752c427cdfba83cbf31359ad7c1ddaac40f
Jaime Iglesias Blanco <jaimeiglesias@outlook.com>
2023-04-25 00:07:39 +0200

    Added base structure for python packages

---
+++ cems2/cloud_analytics/collector/plugins/__init__.py
@@ -0,0 +1,11 @@
+""" 
+Cloud Analytics Collectors 
+"""
+
+from cloud_analytics.collector.plugins.openstack_utilization_collector import (
+    OpenStackUtilizationCollector,
+)
+
+__all__ = [
+    "OpenStackUtilizationCollector",
+]
---
+++ cems2/cloud_analytics/reporter/plugins/__init__.py
@@ -0,0 +1,9 @@
+""" 
+Cloud Analytics Reporters
+"""
+
+from cloud_analytics.reporter.plugins.granafa_reporter import GrafanaReporter
+
+__all__ = [
+    "GrafanaReporter",
+]
