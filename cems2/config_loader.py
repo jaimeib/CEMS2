b23c7db4f569f4029cdc1891e446b258fe413e7b
Jaime Iglesias Blanco <jaimeiglesias@outlook.com>
2023-04-28 16:50:50 +0200

    Added config parsing from config file

---
+++ cems2/config_loader.py
@@ -0,0 +1,13 @@
+"""
+Module for loading configuration files into the system. 
+"""
+
+import os
+from configparser import ConfigParser
+
+path_current_directory = os.path.dirname(__file__)
+path_config_file = os.path.join(path_current_directory, "../etc/cems2/", "cems2.conf")
+
+
+config = ConfigParser()
+config.read(path_config_file)
